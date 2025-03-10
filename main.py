cc7c81d5bd099088fa89e27f1bf842ee0d393541
chachit0865 <chachit0865@gmail.com>
2025-03-10 21:49:44 +0100

    init /scrapper structure

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import logging
+import logging.handlers
+
 from idealista.app.main import IdealistaScraper
 
+logger = logging.getLogger(__name__)
+handler = logging.handlers.TimedRotatingFileHandler("idealista_scraper.log", when="midnight", interval=1)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 def main() -> None:
-    IdealistaScraper(host="0.0.0.0", port=8000)
+    IdealistaScraper(host="0.0.0.0", port=8000) # TODO: this blocks the main thread
+    logger.info("Idealista scraper started")
 
 
 if __name__ == "__main__":
