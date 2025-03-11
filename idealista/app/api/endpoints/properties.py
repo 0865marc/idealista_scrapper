a1a5c58095f0b604536a73d4bcb331a39ef427a3
0865marc <chachit0865@gmail.com>
2025-03-11 10:30:01 +0100

    Refactor entry points + create RedisManager

--- idealista/app/api/endpoints/properties.py
+++ idealista/app/api/endpoints/properties.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.get("/{property_id}")
 def read_property(property_id: int):
     return {"property_id": property_id}
--- idealista/app/api/endpoints/users.py
+++ idealista/app/api/endpoints/users.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.get("/{user_id}")
 def read_user(user_id: int):
     return {"user_id": user_id}
