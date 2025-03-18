7fcc92463badbc33894c54660d908adfecd38b97
0865marc <chachit0865@gmail.com>
2025-03-18 17:14:53 +0100

    remove unused imports from api/routers/router.py and change get("/") response

--- src/api/routers/router.py
+++ src/api/routers/router.py
@@ -1,10 +1,8 @@
 import logging
-import os
 
 from fastapi import APIRouter
 
-from src.api.routers import properties
-from src.api.routers import users
+from src.api.routers import properties, users
 from src.tasks.tasks import get_properties_from_list_page
 
 logger = logging.getLogger(__name__)
@@ -16,7 +14,7 @@ router.include_router(properties.router, prefix="/properties", tags=["properties
 
 @router.get("/")
 def read_root():
-    _properties = get_properties_from_list_page.delay(
+    task = get_properties_from_list_page.delay(
         "https://www.idealista.com/venta-viviendas/lleida-lleida/?ordenado-por=fecha-publicacion-desc"
-        )
-    return {"message": "okay"}
+    )
+    return {"message": task.id}
