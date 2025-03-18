ece2c51cf9a2a688fbefdc7465037a9d7b6d2e16
0865marc <chachit0865@gmail.com>
2025-03-18 17:11:40 +0100

    format celery.py

--- src/core/celery.py
+++ src/core/celery.py
@@ -4,14 +4,14 @@ from celery import Celery
 
 logger = logging.getLogger("src.tasks.celery")
 
-celery_app = Celery('app')
+celery_app = Celery("app")
 celery_app.conf.update(
     broker_url="redis://redis:6379/",
     result_backend="redis://redis:6379/",
-    timezone='Europe/Madrid',
+    timezone="Europe/Madrid",
     enable_utc=True,
     worker_hijack_root_logger=False,
-    broker_connection_retry_on_startup=True
+    broker_connection_retry_on_startup=True,
 )
 celery_app.autodiscover_tasks(["src.tasks"])
 logger.info("Celery inicializado correctamente")
