60a12132829b912651c9e9e148b090175c3b795b
0865marc <0865marc@gmail.com>
2025-03-18 17:10:05 +0100

    remove unused imports and format properly api/main.py

--- src/api/main.py
+++ src/api/main.py
@@ -1,24 +1,24 @@
 import logging
+import logging.handlers
 import os
 
-import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-from celery import Celery
 
 from src.api.routers.router import router
-
 
 logger = logging.getLogger(__name__)
 handler = logging.handlers.TimedRotatingFileHandler(
     "logs/api/api.log",
-    when="midnight", 
+    when="midnight",
     interval=1,
 )
 handler.setFormatter(
-    logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                      datefmt="%Y-%m-%d %H:%M:%S",)
+    logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
+)
 logger.addHandler(handler)
 logger.setLevel(os.getenv("LOG_LEVEL", "INFO"))
 
@@ -29,6 +29,7 @@ app = FastAPI(
 )
 app.include_router(router)
 
+
 # Middleware to log requests and responses
 @app.middleware("http")
 async def log_requests(request: Request, call_next):
@@ -38,10 +39,9 @@ async def log_requests(request: Request, call_next):
     logger.info(f"Respuesta enviada: {response.status_code}")
     return response
 
+
 @app.exception_handler(Exception)
 async def handle_exceptions(request: Request, exc: Exception):
     """Maneja excepciones inesperadas y devuelve un error 500."""
     logger.error(f"Error inesperado: {exc}")
     return JSONResponse(status_code=500, content={"error": "Unexpected error"})
-
-
